68f3961fa446c596452c4f92349f4af1972b9ff8
Toni Kuzmanovski <toni.kuzmanovski@devoteam.com>
2022-11-25 16:28:07 +0100

    Update doceker file and script

--- serverless-shell-scripts/script.sh
+++ serverless-shell-scripts/script.sh
@@ -1,37 +1,62 @@
 #!/bin/bash
 
-echo "Environment variables list"
-echo $GKE
-echo $NODEPOOLNAME
-echo $ZONE
-echo $PROJECT
-
-# Disable autoscaling
-gcloud container clusters update $GKE \
-    --no-enable-autoscaling \
-    --node-pool=$NODEPOOLNAME \
-    --zone=$ZONE \
-    --project=$PROJECT
-
-# Print script results
-RESULT=$?
-if [ $RESULT -eq 0 ]; then
-echo "--------------Disabling GKE autoscaling finished sucesfully---------------"
+if [ -z $STOP ]
+then
+  echo "Environment variable STOP is empty; Setting default value to true"
+  STOP="true"
+  echo $STOP
 else
-  echo "Disabling GKE autoscaling finished with error: $RESULT"
-  exit 1
+  echo "Environment variable STOP is set and it has value:"
+  echo $STOP
 fi
 
-# Resize nodepool to 0
-gcloud container clusters resize -q $GKE \
-    --node-pool $NODEPOOLNAME \
-    --num-nodes 0 --zone=$ZONE \
-    --project=$PROJECT
+if [[ $STOP = "true" ]]
+then
+  # Disable autoscaling
+  gcloud container clusters update $GKE \
+      --no-enable-autoscaling \
+      --node-pool=$NODEPOOLNAME \
+      --zone=$ZONE \
+      --project=$PROJECT
 
-# Print script results
-if [ $RESULT -eq 0 ]; then
-echo "--------------Scaling GKE nodepool to 0 finished sucesfully---------------"
-else
-  echo "Scaling GKE nodepool to 0 finished with error: $RESULT"
-  exit 1
+  # Print script results
+  RESULT=$?
+  if [ $RESULT -eq 0 ]; then
+  echo "--------------Disabling GKE autoscaling finished sucesfully---------------"
+  else
+    echo "Disabling GKE autoscaling finished with error: $RESULT"
+    exit 1
+  fi
+
+  # Resize nodepool to 0
+  gcloud container clusters resize -q $GKE \
+      --node-pool $NODEPOOLNAME \
+      --num-nodes 0 --zone=$ZONE \
+      --project=$PROJECT
+
+  # Print script results
+  if [ $RESULT -eq 0 ]; then
+  echo "--------------Scaling GKE nodepool to 0 finished sucesfully---------------"
+  else
+    echo "Scaling GKE nodepool to 0 finished with error: $RESULT"
+    exit 1
+  fi
+elif [[ $STOP = "false" ]]
+then
+  # Enable autoscaling
+  gcloud container clusters update $GKE \
+      --enable-autoscaling \
+      --min-nodes 0 \
+      --max-nodes 4 \
+      --zone=$ZONE \
+      --project=$PROJECT
+      
+  # Print script results
+  RESULT=$?
+  if [ $RESULT -eq 0 ]; then
+  echo "--------------Disabling GKE autoscaling finished sucesfully---------------"
+  else
+    echo "Disabling GKE autoscaling finished with error: $RESULT"
+    exit 1
+  fi
 fi
